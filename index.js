a3c582c34e257790a644c430a2ed67d259bd4355
almog <almogluzon@gmail.com>
2025-03-19 18:43:51 +0200

    more libs and cleanup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-//Libs used: express, body-parser, mysql2
+//Libs used: express, body-parser, mysql2, path
 const port = 3000;
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 app.use(express.json());
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.listen(port, () => {
+        console.log(`Listening on port: ${port}` );
+});
